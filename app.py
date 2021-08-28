45193c6d94199dade937f7f336bdf2d4f6cd029c
Pascal Kien <pascal.rithi@gmail.com>
2021-08-28 20:09:45 +0200

    Update main python code

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from bson import json_util
 import json
 import pymongo
 from flask import Flask, jsonify
+from flask_cors import CORS
+from mal import MangaSearch,Manga #Librairy use for add Manga data by Web scraping MyAnimeList Website data
 
 ID = os.getenv('ID_MONGO')
 PASS = os.environ.get('PASS_MONGO')
 
 DATABASE_URL =f'mongodb+srv://{ID}:{PASS}@cluster0.sp2ay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+"Local Mongo Database"
+#DATABASE_URL = 'mongodb://localhost:27017/'
 
 client=pymongo.MongoClient(DATABASE_URL)
 mydb = client['Manga_info']
@@ -15,6 +19,7 @@ mycol = mydb['manga_collection']
 mycol2 = mydb["explain_moment"]
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def detail():
@@ -45,3 +50,36 @@ def getSoloManga(id):
 
 if __name__ == "__main__":
     app.run()
+
+
+"""
+Add Manga in database " mycol" , write information in terminal
+nb = 0
+scan = input('enter manga name :')
+def getInfo(scan):
+        search = MangaSearch(scan)
+        manga_id = search.results[nb].mal_id
+        PlusInfo = Manga(manga_id)
+        autor = PlusInfo.authors
+        resume = PlusInfo.synopsis
+        title = search.results[nb].title
+        img = search.results[nb].image_url
+        score = search.results[nb].score
+        synopsis = search.results[nb].synopsis
+        genre = input('enter genre :')
+        print(manga_id,'\n', autor, '\n', title, '\n', img, '\n', score, '\n', synopsis, '\n',genre)
+        mydict = {"id": manga_id,"name":title,"autor":autor,"url_img":img,"score":score,"synopsis":resume,"genre":genre}
+        x = mycol.insert_one(mydict)
+def new_manga():
+        continuer = True
+        while continuer :
+                choix = input("Do you want to continue ? o/n: ")
+                if choix == ('o'):
+                        new_manga = input("enter new manga :")
+                        getInfo(new_manga)
+                else:
+                        print("End Program")
+                        continuer = False
+getInfo(scan)
+new_manga()
+"""
