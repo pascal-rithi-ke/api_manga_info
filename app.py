ea6ed78cf60d743ef51cc3a098d7e479e52b7af9
Pascal Kien <pascal.rithi@gmail.com>
2021-11-21 16:27:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import os
+import pymongo
 from bson import json_util
 import json
-import pymongo
 from flask import Flask, jsonify
 from flask_cors import CORS
-from mal import MangaSearch,Manga #Librairy use for add Manga data by Web scraping MyAnimeList Website data
+#from mal import MangaSearch,Manga #Librairy use for add Manga data by Web scraping MyAnimeList Website data
+
+
 
 ID = os.getenv('ID_MONGO')
 PASS = os.environ.get('PASS_MONGO')
 
 DATABASE_URL =f'mongodb+srv://{ID}:{PASS}@cluster0.sp2ay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-"Local Mongo Database"
 #DATABASE_URL = 'mongodb://localhost:27017/'
 
 client=pymongo.MongoClient(DATABASE_URL)
@@ -32,7 +33,7 @@ def detail():
         response.headers.add("Access-Control-Allow-Origin","*")
         return response
 
-@app.route('/<id>/')
+@app.route('/<id>')
 def getSoloManga(id):
         #Search More info Manga By Id
         mydoc = mycol2.find({'id':id})
@@ -48,3 +49,34 @@ def getSoloManga(id):
 
 if __name__ == "__main__":
     app.run()
+
+
+#Add Manga in database " mycol" , write information in terminal
+#nb = 0
+#scan = input('enter manga name :')
+#def getInfo(scan):
+#        search = MangaSearch(scan)
+#        manga_id = search.results[nb].mal_id
+#        PlusInfo = Manga(manga_id)
+#        autor = PlusInfo.authors
+#        resume = PlusInfo.synopsis
+#        title = search.results[nb].title
+#        img = search.results[nb].image_url
+#        score = search.results[nb].score
+#        synopsis = search.results[nb].synopsis
+#        genre = input('enter genre :')
+#        print(manga_id,'\n', autor, '\n', title, '\n', img, '\n', score, '\n', synopsis, '\n',genre)
+#        mydict = {"id": manga_id,"name":title,"autor":autor,"url_img":img,"score":score,"synopsis":resume,"genre":genre}
+#        x = mycol.insert_one(mydict)
+#def new_manga():
+#        continuer = True
+#        while continuer :
+#                choix = input("Do you want to continue ? o/n: ")
+#                if choix == ('o'):
+#                        new_manga = input("enter new manga :")
+#                        getInfo(new_manga)
+#                else:
+#                        print("End Program")
+#                        continuer = False
+#getInfo(scan)
+#new_manga()
